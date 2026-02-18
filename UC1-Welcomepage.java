e544c687747e0a63d43b1d102d91d5f41dcc1deb
Dragonite2411 <muhammedadnanabdullah@gmail.com>
2026-02-18 15:59:09 +0530

    Add welcome page for Palindrome Checker System

---
+++ UC1-Welcomepage.java
@@ -0,0 +1,10 @@
+//version 1.0
+//author : Adnan Abdullah
+//usecase 1: Welcome page
+public class UC1-Welcomepage {
+    public static void main (String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
